50aeb5824024dd717ae38947aa2428fbc5a4a996
Vedant Dwivedi <117095684+vedant-dwivedi@users.noreply.github.com>
2024-04-15 15:41:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count the elements/count-the-elements.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		// looping through all testcases
+		while(testcases-- > 0){
+		    int n=Integer.parseInt(br.readLine());
+		    String line1 = br.readLine();
+		    String line2 = br.readLine();
+		    String[] a1 = line1.trim().split("\\s+");
+		    String[] b1 = line2.trim().split("\\s+");
+		    int a[]=new int[n];
+		    int b[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        a[i]=Integer.parseInt(a1[i]);
+		        b[i]=Integer.parseInt(b1[i]);
+		    }
+		    int q = Integer.parseInt(br.readLine());
+		    int query[]=new int[q];
+		    for(int i=0;i<q;i++)
+		    {
+		        query[i]=Integer.parseInt(br.readLine());
+		    }
+		    Solution ob=new Solution();
+		    int ans[]=ob.countElements(a,b,n,query,q);
+		    for(int i=0;i<q;i++)
+		    System.out.println(ans[i]);
+		    
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public static int[] countElements(int a[], int b[], int n, int query[], int q)
+    {
+     int[] arr= new int[100001];
+     
+     for(int i=0;i<n;i++){
+         arr[b[i]]++;
+     }
+     
+     for(int i=1;i<100001;i++){
+         arr[i]=arr[i]+arr[i-1];
+     }
+     
+     int[] soln= new int[q];
+     
+     for(int i=0;i<q;i++){
+         soln[i]= arr[a[query[i]]];
+     }
+     
+     return soln;
+    }
+}
